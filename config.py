a2289f527f540d1ea594b06177018a5c4748adcb
RALD <39939024+yrald47@users.noreply.github.com>
2023-10-31 04:54:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,12 @@
 from pymongo import MongoClient
 
-hostname = '172.16.100.212'
+hostname = ''
 port = 27017  # Default MongoDB port
-username = 'ictdata'  # If authentication is required
-password = 'P@ssw0rd'  # If authentication is required
+username = ''  # If authentication is required
+password = ''  # If authentication is required
 
 # Create a MongoClient instance
 client = MongoClient(hostname, port, username=username, password=password)
 
-db = client['sentiment']
-collection = db['news']
-
-'''
-{
-    id: "",
-    title: "",
-    link: "",
-    content: "",
-    negative_score: "",
-    neutral_score: "",
-    positive_score: "",
-    compound_score: ""
-}
-'''
+db = client['your_db']
+collection = db['collection_name']
