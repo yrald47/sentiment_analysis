9f21cf5887e8136b390b8391f3a976c64b242e38
adriantom9 <adriantomrb@gmail.com>
2023-11-21 17:00:46 +0700

    tokenisasi
    next: filter kata-kata selain kata kerja dan benda (cth: ini, itu, jadi)

--- sentiment.py
+++ sentiment.py
@@ -1,9 +1,18 @@
 import config
+import pandas as pd
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory# create stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()# stemming process
 
 collection = config.db['news']
 for post in collection.find({'content': {'$ne': ''}}).limit(50):
     if post['keyword'] in post['content'].lower():    
         print(post['title'], post['keyword'])
 
-print(collection.find_one({'title':'direktur bank mandiri blak-blakan alasan borong saham rp 2 m'}))
-print(type(collection.find_one({'title':'direktur bank mandiri blak-blakan alasan borong saham rp 2 m'})))
+doc = collection.find_one({'title':'direktur bank mandiri blak-blakan alasan borong saham rp 2 m'})
+output   = stemmer.stem(doc['content'])
+token = output.split()
+df = pd.DataFrame(token)
+print(df[0].value_counts().to_frame())
+#print(df)
+
