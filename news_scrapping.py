3ccec9a260e8f6b09935d1cdb1acb1760057d4eb
adriantom9 <adriantomrb@gmail.com>
2023-11-21 16:37:41 +0700

    fixed news scrapping
    - filter out "lihat juga"
    - add space for each paragraph

--- news_scrapping.py
+++ news_scrapping.py
@@ -24,7 +24,10 @@ def getLinks(tag_search, list_news_tag, list_news_tag_class, news_link_class):
         links = []
         for news in news_list:
             link = news.find('a', href=True)['href'] if news_link_class == "" else news.find('a', news_link_class, href=True)['href']
-            links.append(link)
+            collection = config.db['news']
+            result = collection.count_documents({"link": {"$regex": re.compile(link[:75])}})
+            if result == 0:
+                links.append(link)
         return links
     except Exception as e:
         return []
@@ -38,8 +41,8 @@ def getContent2(link, content_tag, content_class, paragraph_tag, paragraph_class
         paragraphs = []
         for index, classes in enumerate(content_class):
             # index = content_class.index(classes)
-            print("INDEX: " + str(index) )
-            print("<" + content_tag[index] + " class='" + classes + "'><" + paragraph_tag[index] + " class='" + paragraph_class[index] + "'></" + paragraph_tag[index] + "></" + content_tag[index] + ">")
+            # print("INDEX: " + str(index) )
+            # print("<" + content_tag[index] + " class='" + classes + "'><" + paragraph_tag[index] + " class='" + paragraph_class[index] + "'></" + paragraph_tag[index] + "></" + content_tag[index] + ">")
             # print(news_lxml.find(content_tag[index], tag))
             try:
                 # content = news_lxml.find(content_tag[index], classes)
@@ -51,13 +54,13 @@ def getContent2(link, content_tag, content_class, paragraph_tag, paragraph_class
         # print(paragraphs)
         # title = news_lxml.find("title").get_text()
         title = news_lxml.title.text
-        print(title, type(title))
+        # print(title, type(title))
         
         str_date = ""
         news_date = dt.now(pytz.timezone('Asia/Jakarta'))
         locale.setlocale(locale.LC_ALL , 'id_ID')
         for index, tags in enumerate(news_date_tag):
-            print(str(index) + ": <" + tags + " class='" + news_date_class[index] + "'>")
+            # print(str(index) + ": <" + tags + " class='" + news_date_class[index] + "'>")
             try:
                 str_date = news_lxml.find(tags, news_date_class[index]).get_text()
                 # str_date = str_date.split(date_string_splitter)[1].split('WIB')[0].strip()
@@ -70,26 +73,24 @@ def getContent2(link, content_tag, content_class, paragraph_tag, paragraph_class
                 print("Date Error: " + str(e))
                 continue
 
-        print("STR DATE: " + str_date[0])
+        # print("STR DATE: " + str_date[0])
         # print(type(str_date))
         # print(len(str_date))
-        print("NEWS_DATE: " + str(news_date))
+        # print("NEWS_DATE: " + str(news_date))
 
         context = ""
         for paragraph in paragraphs:
             # print("PARAGRAPH LEN: " + str(len(paragraph)))
-            context += paragraph.get_text()
+            if paragraph.get_text().find('Baca Juga:') == -1 and paragraph.get_text().find('Baca juga:') == -1 and paragraph.get_text().find('Pilihan Editor:') == -1 and paragraph.get_text().find('Lihat juga:') == -1 and paragraph.get_text().find('Lihat Juga:') == -1:
+                context += paragraph.get_text()
+                context += " "
 
-        print("===CONTEXT===")
-        print(context.encode('utf-8'))
-        
-        jakarta = pytz.timezone('Asia/Jakarta')
-        dt_now = dt.now(jakarta)
-        scraping_date = datetime.datetime.strptime(dt_now.strftime("%Y-%m-%d"), "%Y-%m-%d")
+        # print("===CONTEXT===")
+        # print(context.encode('utf-8'))
 
-        return scraping_date, news_date, title, context
-    except URLError as e:
-        return e
+        return {'title': title.lower(), 'news_date': news_date, 'content': context.encode('utf-8'), 'error': ''.encode('utf-8')}
+    except Exception as e:
+        return {'title': '', 'news_date': '', 'content': str(e).encode('utf-8'), 'error': str(e).encode('utf-8')}
 
     # collection = config.db['news']
     # scrapped_news = {"scraping_date": scraping_date, "title": hash(title.lower()), "link": link, "content": context}
--- sentiment.py
+++ sentiment.py
@@ -4,3 +4,6 @@ collection = config.db['news']
 for post in collection.find({'content': {'$ne': ''}}).limit(50):
     if post['keyword'] in post['content'].lower():    
         print(post['title'], post['keyword'])
+
+print(collection.find_one({'title':'direktur bank mandiri blak-blakan alasan borong saham rp 2 m'}))
+print(type(collection.find_one({'title':'direktur bank mandiri blak-blakan alasan borong saham rp 2 m'})))
